fe003bab478f8812f62569c9b42e5579123a8fa6
Tusman917 <tusman2018@gmail.com>
2022-04-14 18:02:28 -0400

    Fix the css classes issue in the creditcard component

--- src/components/creditCard/ShowCardButton.js
+++ src/components/creditCard/ShowCardButton.js
@@ -1,7 +1,7 @@
 import classes from "./ShowCardButton.module.css";
 const ShowCardButton = (props) => {
   return (
-    <div>
+    <div className={classes}>
       <button style={{ borderRadius: '100px', marginBottom: '2.5rem', height: 35, paddingTop: 5 }} onClick={props.onClick}>SHOW CARD INFORMATION</button>
     </div>
   );
